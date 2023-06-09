f392f4ae00b3da9cef7ca2099317bca0766a0450
youngeun <jessicak0828@gmail.com>
2023-06-09 23:41:38 +0900

    feat: Select 컴포넌트 selected 라는 불린값 상태 주기
    true, false 에 따라서 Select 컴포넌트의 UI 가 바뀐다

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -4,7 +4,17 @@ import React, {useState} from 'react';
 
 function Select({children}) {
   const [selected, setSelected] = useState(false)
-  const selectClickHandler = () => {}
+
+  const selectClickHandler = () => {
+    if(selected===false){
+      setSelected(true)
+      
+    } else {
+      setSelected(true)
+    }
+  }
+  
+  
   return (
   <li className={selected? 'selectItem selected': 'selectItem'} onClick={selectClickHandler}>{children}</li>)
 }
