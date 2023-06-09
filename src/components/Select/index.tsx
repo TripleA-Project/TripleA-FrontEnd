fc17076fcdad6d0648eb8990be0a0bf0ecd955d8
youngeun <jessicak0828@gmail.com>
2023-06-09 23:38:28 +0900

    feat: Select 컴포넌트 기본 구조 만들기

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,7 +1,12 @@
 'use client';
 
-function Select() {
-  return <>Select UI</>;
+import React, {useState} from 'react';
+
+function Select({children}) {
+  const [selected, setSelected] = useState(false)
+  const selectClickHandler = () => {}
+  return (
+  <li className={selected? 'selectItem selected': 'selectItem'} onClick={selectClickHandler}>{children}</li>)
 }
 
 export default Select;
