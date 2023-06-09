a3d49c1d0f99d50094dc5f190743f4c3f27dd81a
youngeun <jessicak0828@gmail.com>
2023-06-10 00:07:51 +0900

    feat: selectedArray, setSelectedArray 를 props 로 받기

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -2,21 +2,26 @@
 
 import React, {useState} from 'react';
 
-function Select({children}) {
+function Select({children, ...props}) {
+  
   const [selected, setSelected] = useState(false)
+  const selectedArray = props.selectedArray
+  const setSelectedArray= props.setSelectedArray   
 
-  const selectClickHandler = () => {
+  const clickHandler = (e:MouseEvent) => {
     if(selected===false){
       setSelected(true)
-      
+      setSelectedArray([...selectedArray, e.target.innerText])
     } else {
       setSelected(true)
+      const filteredArray = selectedArray.filter((item) => item !== e.target.innerText)
+      setSelectedArray(filteredArray)
     }
   }
   
   
   return (
-  <li className={selected? 'selectItem selected': 'selectItem'} onClick={selectClickHandler}>{children}</li>)
+  <li className={selected? 'selectItem selected': 'selectItem'} onClick={clickHandler}>{children}</li>)
 }
 
 export default Select;
