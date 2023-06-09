6254b959476c13d5824f3d668d56844f9d33e2b9
youngeun <jessicak0828@gmail.com>
2023-06-10 01:28:48 +0900

    design & feat: Select 컴포넌트 스타일 적용 및 타입 추가

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import React, {useState, MouseEvent, MouseEventHandler} from 'react';
-
+import styled from '@emotion/styled';
 
 interface SelectComponentProps {
   children : string;
   selectedArray : string[] | [];
   setSelectedArray: any;
 }
-
+interface NewsCategoryItemProps {
+  selected: boolean;
+}
 
 function Select({children, ...props}:SelectComponentProps) {
   
@@ -18,11 +20,10 @@ function Select({children, ...props}:SelectComponentProps) {
 
   const clickHandler:MouseEventHandler = (e:MouseEvent) => {
     console.log(e.target.innerHTML)
+    setSelected(!selected)
     if(selected===false){
-      setSelected(true)
       setSelectedArray([...selectedArray, e.target.innerHTML])
     } else {
-      setSelected(true)
       const filteredArray:string[]|[] = selectedArray.filter((item) => item !== e.target.innerText)
       setSelectedArray(filteredArray)
     }
@@ -30,7 +31,18 @@ function Select({children, ...props}:SelectComponentProps) {
   
   
   return (
-  <li className={selected? 'selectItem selected': 'selectItem'} onClick={clickHandler}>{children}</li>)
+  <NewsCategoryItem className={selected? 'selectItem selected': 'selectItem'} onClick={clickHandler} selected={selected}>{children}</NewsCategoryItem>)
 }
 
 export default Select;
+
+//style
+const NewsCategoryItem = styled.li<NewsCategoryItemProps>`
+  display: inline-block;
+  border: 1px solid ${({selected})=> selected ? '#6A6A6A': '#000'};
+  border-radius: 25px;
+  margin: 5px;
+  padding: 10px 20px;
+  background-color:${({selected})=>selected? '#6A6A6A' : '#E0E0E0'};
+  color: #fff;
+`
