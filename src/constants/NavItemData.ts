673f5b7f3f86bc6cf1804ebd41b6bfc96bdeca07
seon-mikim <tjsalsun@gmail.com>
2023-06-12 21:32:41 +0900

    fix: 상수데이터 pathName 변경
    -'/new'=> '/' pathName 변경
    - '/my' => '/me' pathName 변경

--- src/constants/NavItemData.ts
+++ src/constants/NavItemData.ts
@@ -5,9 +5,9 @@ import { HiUser } from 'react-icons/hi';
 import { IoNewspaperOutline } from 'react-icons/io5';
 
 export const NavItemData = [
-  { pathName: '/news', name: '뉴스', Icon: HiHome },
+  { pathName: '/', name: '뉴스', Icon: HiHome },
   { pathName: '/read', name: '내가 본', Icon: IoNewspaperOutline },
   { pathName: '/chart', name: '차트', Icon: HiChartBar },
   { pathName: '/search', name: '검색', Icon: HiSearch },
-  { pathName: '/my', name: 'MY', Icon: HiUser },
+  { pathName: '/me', name: 'MY', Icon: HiUser },
 ];
