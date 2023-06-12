f57050cff68fcec64ca953f3bf224a21af6ca364
youngeun <jessicak0828@gmail.com>
2023-06-12 23:14:02 +0900

    feat: Modal 컴포넌트 구조 생성
    - Modal 컴포넌트를  더 작은 컴포넌트 단위로 쪼개기 => Title, Content, Button, SubButton

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,5 +1,43 @@
+"use client"
+import { useState } from "react";
+import styled from "@emotion/styled";
+
+const ModalContainer = styled.div`
+  border-radius: 15px;
+  width: ${({size})=> size === 'small'? '215px' : '390px'};
+  height: ${({size})=>size === 'small'? '148px' : '357px'};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`
+
+const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 20px;
+  font-weight: bold; //semibold
+`
+const Content = styled.p`
+  font-size: 16px;
+  text-align: center;
+
+`
+
 function Modal() {
-  return <div>Modal</div>;
+  const [modalSize, setModalSize] = useState('')
+
+
+
+  return (
+  <ModalContainer size={modalSize}>
+    <Title>title</Title>
+    <Content>content</Content>
+    <div>button</div>
+    <div>subbutton</div>
+  </ModalContainer>
+  )
 }
 
 export default Modal;
