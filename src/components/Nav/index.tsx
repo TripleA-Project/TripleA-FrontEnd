d9ad1b8a474d831002994112c98e868fc5e10522
seon-mikim <tjsalsun@gmail.com>
2023-06-12 22:59:15 +0900

    refator: export default 부분 가독성을 위해 변경
    - 함수명 변경 Navbar => Nav 변경 (7번째 코드)
    - 18번째 코드 추가 export default Nav;

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -4,13 +4,15 @@ import { usePathname } from 'next/navigation';
 import NavItem from './NavItem';
 import { NavItemData } from '@/constants/NavItemData';
 
-export default function Navbar() {
+function Nav() {
   const pathname = usePathname();
-  const notRenderNavItem = pathname !== '/signup' && pathname !== '/login';
+  const notRenderNav = pathname !== '/signup' && pathname !== '/login';
 
   return (
     <div className="fixed bottom-0 mb-4 flex w-screen justify-between text-center">
-      {notRenderNavItem && NavItemData.map((navItem) => <NavItem navItem={navItem} key={navItem.pathName} />)}
+      {notRenderNav && NavItemData.map((navItem) => <NavItem navItem={navItem} key={navItem.pathName} />)}
     </div>
   );
 }
+
+export default Nav;
