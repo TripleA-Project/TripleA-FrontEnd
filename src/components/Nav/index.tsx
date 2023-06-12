f58d146f472cc4b32d24178dbd2c7f83391505ea
seon-mikim <tjsalsun@gmail.com>
2023-06-12 22:05:10 +0900

    feat: Nav 컴포넌트 생성 및 구현
    - Nav 컴포넌트 생성(client component)
    - usePathname Hook을 이용해 '/signup'과 '/signIn'에서
      해당 컴포넌트 나오지 않게 조건부 렌더링 처리
    - NavItemData constants를 이용하여 map메소드를 이용하여
      데이터를  NavItem 컴포넌트에 전달

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,7 +1,16 @@
 'use client';
 
-function Nav() {
-  return <div>Nav UI</div>;
-}
+import { usePathname } from 'next/navigation';
+import NavItem from './NavItem';
+import { NavItemData } from '@/constants/NavItemData';
 
-export default Nav;
+export default function Navbar() {
+  const pathname = usePathname();
+  const notRenderNavItem = pathname !== '/signup' && pathname !== '/login';
+
+  return (
+    <div className="fixed bottom-0 mb-4 flex w-screen justify-between text-center">
+      {notRenderNavItem && NavItemData.map((navItem) => <NavItem navItem={navItem} key={navItem.pathName} />)}
+    </div>
+  );
+}
