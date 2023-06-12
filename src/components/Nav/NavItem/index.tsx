caa31799543802a350ef23c3083cdbe2cf3b8026
seon-mikim <tjsalsun@gmail.com>
2023-06-12 22:38:38 +0900

    feat: NavItem 컴포넌트 생성 및 구현
    - NavItemProps interface 생성(client component)
    - Nav 컴포넌트에서 내려준 데이터 적용처리
    - NavItem Link 컴포넌트 누를 시 해당 페이지로 라우팅 처리

---
+++ src/components/Nav/NavItem/index.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { IconType } from 'react-icons/lib';
+
+interface NavItemProps {
+  navItem: {
+    pathName: string;
+    name: string;
+    Icon: IconType;
+  };
+}
+
+export default function NavItem({ navItem }: NavItemProps) {
+  const pathName = usePathname();
+
+  return (
+    <div key={navItem.pathName} className="mx-auto">
+      <Link href={navItem.pathName} className={`${pathName === navItem.pathName ? 'text-black' : 'text-neutral-400'}`}>
+        <div className="h-10 w-10">
+          <navItem.Icon className="mx-auto text-center text-xl" />
+          <span className="text-center text-[10px] font-bold">{navItem.name}</span>
+        </div>
+      </Link>
+    </div>
+  );
+}
