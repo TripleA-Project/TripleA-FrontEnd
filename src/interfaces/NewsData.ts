8d44fe275ca6e3e1e6c878f3d91befae2d6bee94
youngeun <jessicak0828@gmail.com>
2023-06-13 14:59:48 +0900

    feat: interface NewsData 추가

---
+++ src/interfaces/NewsData.ts
@@ -0,0 +1,15 @@
+export interface NewsData {
+  newsId : number;
+  symbol?: string;
+  logo?:string;
+  source?: string;
+  title: string;
+  description?: string;
+  thumbnail?:string;
+  publishedDate: string;
+  sentiment?:number;
+  bookmark?: {
+    count?: number;
+    isBookmark?: boolean;
+  }     
+}
