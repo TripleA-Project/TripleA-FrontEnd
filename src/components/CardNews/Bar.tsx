191d8d00d678a432fb3f71dd01057f53cd147ad9
youngeun <jessicak0828@gmail.com>
2023-06-13 14:23:34 +0900

    feat: Bar 컴포넌트 생성

---
+++ src/components/CardNews/Bar.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+
+const ColorBar = styled.div`
+width: 5px;
+height: 72px;
+border-radius:5px ;
+background-color: ${({color})=> color};
+`
+
+export default function Bar({color}) {
+  return <ColorBar color={color}/>
+}
