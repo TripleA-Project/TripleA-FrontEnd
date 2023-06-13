2e5b6c40a6eaad97d318702a478fed3044bc860f
youngeun <jessicak0828@gmail.com>
2023-06-13 17:55:00 +0900

    feat: NewsProps 타입 수정

--- src/components/CardNews/News.tsx
+++ src/components/CardNews/News.tsx
@@ -2,38 +2,30 @@ import { NewsData } from '@/interfaces/NewsData'
 
 import { ReactNode } from 'react'
 import Bar from './Bar'
+
 import styled from '@emotion/styled'
 
-
-interface NewsProps extends Partial<NewsData> {
-  hole1: ReactNode;
-  hole2: ReactNode;
+interface NewsProps extends Pick<NewsData,'title'| 'source'| 'publishedDate'| 'sentiment' >{
+  hole1?: ReactNode;
+  hole2?: ReactNode;
 }
 
 const NewsContainer = styled.div`
   display:flex;
 `
-const Hole1 = styled.div`
-`
-const Hole2 = styled.div``
+
 
 export default function News({title, source , publishedDate , sentiment , hole1, hole2 }:NewsProps) {
-
-
   return (
     <NewsContainer>
       <Bar sentiment={sentiment}/>
-      <Hole1>
-        {hole1}
-      </Hole1>
       <div>
+        {hole1 && hole1}
         <h3>{title}</h3>
         <span>{source}</span>
         <span>{publishedDate}</span>
+        {hole2 && hole2}
       </div>
-     <Hole2>
-      {hole2}
-     </Hole2>
     </NewsContainer>
   )
 }
