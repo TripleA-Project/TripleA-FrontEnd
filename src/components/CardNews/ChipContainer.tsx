43c5e805646a21eaf094f5467014c6aa33c97e4a
youngeun <jessicak0828@gmail.com>
2023-06-13 18:18:10 +0900

    feat: ChipContainer Component Props 타입 변경

--- src/components/CardNews/ChipContainer.tsx
+++ src/components/CardNews/ChipContainer.tsx
@@ -1,7 +1,7 @@
 import { NewsData } from "@/interfaces/NewsData"
 
 
-export default function ChipContainer({symbol, logo}:Partial<NewsData>) {
+export default function ChipContainer({symbol, logo}:Pick<NewsData, 'symbol'|'logo'>) {
   return (
     <div>
       <div>{symbol}</div>
