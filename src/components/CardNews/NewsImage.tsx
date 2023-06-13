1adef2c04751b4c9f30469dc2e66818b2cd00d4b
youngeun <jessicak0828@gmail.com>
2023-06-13 14:58:33 +0900

    feat: NewsImage 컴포넌트 Props 타입 지정
    Partial<MewsData>

--- src/components/CardNews/NewsImage.tsx
+++ src/components/CardNews/NewsImage.tsx
@@ -1,17 +1,19 @@
 
 import styled from '@emotion/styled'
+import { NewsData } from '@/interfaces/NewsData';
 
-const ImageContainer = styled.div`
+
+const ImageContainer = styled.div<Partial<NewsData>>`
   width: 66px;
   height: 66px;
-  background-image: url(${({src})=>src});
+  background-image: url(${({thumbnail})=>thumbnail});
   background-repeat: no-repeat;
   background-size: cover;
 `
 
-export default function NewsImage({src}) {
+export default function NewsImage({thumbnail}:Partial<NewsData>) {
   return (
-    <ImageContainer src={src}/>
+    <ImageContainer thumbnail={thumbnail&&thumbnail}/>
   )
 }
 
