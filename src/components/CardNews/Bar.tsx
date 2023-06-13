c24e88aa6053c58b7acd11be3e5f30e0387f5104
youngeun <jessicak0828@gmail.com>
2023-06-13 18:00:16 +0900

    feat: Bar 컴포넌트 감정지수에 따라 색 다르게

--- src/components/CardNews/Bar.tsx
+++ src/components/CardNews/Bar.tsx
@@ -1,12 +1,28 @@
+import {css} from '@emotion/react'
 import styled from '@emotion/styled'
+import { NewsData } from '@/interfaces/NewsData'
 
-const ColorBar = styled.div`
-width: 5px;
-height: 72px;
-border-radius:5px ;
-background-color: ${({color})=> color};
+interface ColorBarProps extends Pick<NewsData, 'sentiment'> {}
+
+const ColorBar = styled.div<ColorBarProps>`
+  width: 5px;
+  height: 72px;
+  border-radius:5px ;
+  background-color: ${({sentiment})=>{
+    if(sentiment>= -10 && sentiment < 0.5){
+     return '#786BE4'
+    } else if (sentiment >= 0.5 && sentiment < 1.5){
+      return '#759DEB'
+    } else if (sentiment >= 1.5 && sentiment < 3.5 ){
+      return '#91DF75'
+    } else if (sentiment >= 3.5 && sentiment < 4.5){
+      return '#F6DD52'
+    } else if (sentiment >= 4.5 && sentiment < 10){
+      return '#FD954A'
+    }}
+};
 `
 
-export default function Bar({color}) {
-  return <ColorBar color={color}/>
+export default function Bar({sentiment}:ColorBarProps) {
+  return <ColorBar sentiment={sentiment}/>
 }
