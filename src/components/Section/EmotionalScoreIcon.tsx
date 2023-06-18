cb6362ca491abeb0317d3c069e257e6e83d7eca3
youngeun <jessicak0828@gmail.com>
2023-06-18 21:53:55 +0900

    feat: Props 타입 추가

--- src/components/Section/EmotionalScoreIcon.tsx
+++ src/components/Section/EmotionalScoreIcon.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { MouseEventHandler } from 'react'
 
 
-export default function EmotionalScore({onClick}: MouseEventHandler) {
+export default function EmotionalScoreIcon({onClick}:{onClick:MouseEventHandler}) {
   return (
     <div className='w-[24px] h-[24px] rounded-[50%] bg-gradient-to-br from-[#FD954A]
  to-[#786BE4] hover:cursor-pointer' onClick={onClick}></div>
