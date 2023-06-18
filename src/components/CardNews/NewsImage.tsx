5b58144eace4b9ca64d5ea49a763c1752b9f112c
youngeun <jessicak0828@gmail.com>
2023-06-18 19:48:19 +0900

    feat: NewsImage 컴포넌트 tailwind CSS로 바꾸기

--- src/components/CardNews/NewsImage.tsx
+++ src/components/CardNews/NewsImage.tsx
@@ -4,19 +4,14 @@ import { NewsData } from '@/interfaces/NewsData';
 
 
 interface NewsImageProps extends Pick<NewsData,'thumbnail'>{
-  cardDirection: 'column' | 'row';
+  cardDirection: string;
 }
 interface ImageContainerProps extends NewsImageProps{
   
 }
 const ImageContainer = styled.div<ImageContainerProps>`
-  overflow: hidden;
-  box-sizing: border-box;
-  width: 100%;
-  border-top-left-radius: 10px ;
-  border-top-right-radius: 10px;
-  border-bottom-left-radius: ${({cardDirection})=> cardDirection === 'column' ? 0 : '10px'};
-  border-bottom-right-radius: ${({cardDirection})=> cardDirection === 'column' ? 0 : '10px'};
+  border-bottom-left-radius: ${({cardDirection})=> cardDirection === 'column' ? 0 : '12px'};
+  border-bottom-right-radius: ${({cardDirection})=> cardDirection === 'column' ? 0 : '12px'};
   background-color: red;
   width: ${({cardDirection})=> cardDirection === 'column' ? '358px':'66px'};
   height: ${({cardDirection})=> cardDirection === 'column' ? '197px':'66px'};
@@ -24,7 +19,7 @@ const ImageContainer = styled.div<ImageContainerProps>`
 
 export default function NewsImage({thumbnail='https://giphy.com/gifs/running-muppets-7kn27lnYSAE9O', cardDirection}:NewsImageProps) {
   return (
-    <ImageContainer thumbnail={thumbnail} cardDirection={cardDirection}>
+    <ImageContainer className='rounded-t-xl overflow-hidden box-border w-full' thumbnail={thumbnail} cardDirection={cardDirection}>
       {/* {thumbnail && <Image loader={'https://giphy.com/gifs/running-muppets-7kn27lnYSAE9O'} src={thumbnail} alt='thumbnail' width={66} height={66}/>} */}
     </ImageContainer>
   )
