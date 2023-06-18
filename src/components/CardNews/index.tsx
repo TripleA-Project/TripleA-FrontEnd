8419f662747c94e044ce4c2229279222f343c2c5
youngeun <jessicak0828@gmail.com>
2023-06-18 19:46:53 +0900

    feat: CardNews 컴포넌트 타입 추가 및 스타일 정리

--- src/components/CardNews/index.tsx
+++ src/components/CardNews/index.tsx
@@ -5,9 +5,13 @@ import ChipContainer from './ChipContainer';
 import News from './News'
 import ButtonContainer from './ButtonContainer';
 
-import { NewsData } from '@/interfaces/NewsData';
+import { CardNewsData, NewsData } from '@/interfaces/NewsData';
 
 import styled from '@emotion/styled';
+
+interface CardNewsProps extends CardNewsData{
+  cardDirection: string;
+}
 
 interface CardProps {
   cardDirection: string; //column, row, tile
@@ -18,37 +22,28 @@ interface CardProps {
 const Card = styled.li<CardProps>`
   overflow:hidden;
   box-sizing: border-box;
-  margin: 10px;
+  max-width: ${({cardDirection})=> cardDirection==='tile' ? '106px' : '770px'};
+  min-width:  ${({cardDirection})=> cardDirection ===`column`? `358px`:( cardDirection === `row` ? `357px` : `106px`)};
+  margin: 10px 0;
   padding: ${({cardDirection})=> cardDirection === `tile` && `12px`};
   border-radius: 16px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  width: ${({cardDirection})=> cardDirection ===`column`? `358px`:( cardDirection === `row` ? `357px` : `106px`)};
+  /* width: ${({cardDirection})=> cardDirection ===`column`? `358px`:( cardDirection === `row` ? `357px` : `106px`)}; */
   min-height: ${({cardDirection})=> cardDirection === `column`?`344px`:( cardDirection === `row` ? `122px` : `auto`)};
   height : ${({cardDirection})=>cardDirection === `tile` && `106px`};
   background-color:${({cardDirection, sentimentColor})=> cardDirection === `tile` ? `${sentimentColor}` : `#fff`} ;
   .title {
-    width: ${({cardDirection})=>cardDirection === `tile` ? `auto` : `106px`};
-    /* height: 106px; */
+    max-height: ${({cardDirection})=> cardDirection === `tile` && `40px`};
     font-size: ${({cardDirection})=> cardDirection === `tile` && `14px`};
     font-weight:${({cardDirection})=>cardDirection === `tile` && `600`};
-    overflow: hidden; /*자르세요*/
-    text-overflow: ellipsis;/* 생략하세요 */
-  }
-  .time {
-    text-align: right;
-    color: #fff;
-    font-size: 10px;
-    font-weight: 600;
   }
 `
 
 
 
-function CardNews({ newsId,symbol,logo, source, title, thumbnail,publishedDate, sentiment, bookmark}:NewsData) {
-
-  const [cardDirection, setCardDirection] = useState('tile')
+function CardNews({ newsId,symbol,logo, source, title, thumbnail,publishedDate, sentiment, bookmark, cardDirection}:CardNewsProps) {
 
   const getSentimentColor = (sentiment:number)=> { 
     if(sentiment >= -10 && sentiment < 0.5){
@@ -61,43 +56,38 @@ function CardNews({ newsId,symbol,logo, source, title, thumbnail,publishedDate, 
       return '#F6DD52'
     } else if (sentiment >= 4.5 && sentiment < 10){
       return '#FD954A'
-    }
+    } return '#000'
   }
   const sentimentColor = getSentimentColor(sentiment)
     
-  
-
-
-
   return (
-    <Card cardDirection={cardDirection} sentimentColor={sentimentColor}>
-      {cardDirection==='column'?
-      <div className='column'>
-        <div >
-          <NewsImage thumbnail={thumbnail} cardDirection={cardDirection}/>
-        </div>
-        <div style={{padding: '10px'}}>
+    <>
+      <Card className='rounded-2xl bg-white' cardDirection={cardDirection} sentimentColor={sentimentColor}>
+        {cardDirection==='column'?
+        <div className='column'>
+          <div >
+            <NewsImage thumbnail={thumbnail} cardDirection={cardDirection}/>
+          </div>
+          <div style={{padding: '10px'}}>
+            <ChipContainer symbol={symbol} logo={logo}/>
+            <News title={title} source={source} publishedDate={publishedDate} sentimentColor={sentimentColor} hole1={''} hole2={''} cardDirection={cardDirection}/>
+            <ButtonContainer newsId={newsId} bookmark={bookmark}/>
+          </div>
+        </div>:
+        cardDirection === 'row' ?
+        <div className='row'>
           <ChipContainer symbol={symbol} logo={logo}/>
-          <News title={title} source={source} publishedDate={publishedDate} sentimentColor={sentimentColor} hole1={''} hole2={''} cardDirection={cardDirection}/>
-          <ButtonContainer newsId={newsId} bookmark={bookmark}/>
+          <News title={title} source={source} publishedDate={publishedDate} sentimentColor={sentimentColor} hole1={<NewsImage thumbnail={thumbnail} cardDirection={cardDirection}/>} hole2={<ButtonContainer bookmark={bookmark} newsId={newsId}/>} cardDirection={cardDirection}/>
         </div>
-      </div>:
-      cardDirection === 'row' ?  
-      <div className='row'>
-        <div className='chipContainer'>
-          <ChipContainer symbol={symbol} logo={logo}/>
-        </div>
-        <div>
-          <News title={title} source={source} publishedDate={publishedDate} sentimentColor={sentimentColor} hole1={<NewsImage thumbnail={thumbnail}/>} hole2={<ButtonContainer bookmark={bookmark} newsId={newsId}/>} cardDirection={cardDirection}/>
-        </div>
-      </div>
-      :
-      <>
-        <div className='title'>{title}</div>
-        <div className='time'>{publishedDate}</div>
-      </>
-      }
-    </Card>
+        :
+        <>
+          <div className='title  overflow-hidden text-ellipsis '>{title}</div>
+          <div className='time text-right text-[10px] font-semibold text-white'>{publishedDate}</div>
+        </>
+        }
+      </Card>
+    {cardDirection === 'row' &&   <div className='divider w-[100%] h-[1.5px] bg-[#eee]'></div>}
+    </>
   )
 }
 
