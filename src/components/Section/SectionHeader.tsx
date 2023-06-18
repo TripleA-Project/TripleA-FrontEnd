d347fd11e4cf496cb82ba089f7c45fac9bd044c0
youngeun <jessicak0828@gmail.com>
2023-06-18 21:48:54 +0900

    feat: Section 타입에 따라 CardNews 컴포넌트 다르게 렌더시키기

--- src/components/Section/SectionHeader.tsx
+++ src/components/Section/SectionHeader.tsx
@@ -1,18 +1,20 @@
 "use client"
-import React, {ReactNode, useState} from 'react'
+import React, {ReactNode, SetStateAction, useState} from 'react'
 import styled from '@emotion/styled'
 import { IconType } from 'react-icons'
 import {BiNews} from 'react-icons/bi'
 import ButtonContainer from '@/components/Button/ButtonContainer'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { BsGrid3X3GapFill } from 'react-icons/bs'
-import EmotionalScore from './EmotionalScoreIcon'
+import EmotionalScoreIcon from './EmotionalScoreIcon'
+import EmotionalScoreModal from '../Modal/EmotionalScoreModal'
 
 
 interface SectionHeaderProps {
+  type: 'hotNews'|'currentNews',
   icon: ReactNode,
   title: string,
-
+  setCardDirection?: Function
 }
 
 const SectionHeaderContainer = styled.div`
@@ -26,23 +28,46 @@ const SectionHeaderContainer = styled.div`
   }
 `
 
-export default function SectionHeader({icon, title}:SectionHeaderProps) {
+export default function SectionHeader({type, icon, title, setCardDirection}:SectionHeaderProps) {
     const [isClicked1, setIsClicked1] = useState(false)
     const [isClicked2, setIsClicked2] = useState(false)
     const [isClicked3, setIsClicked3] = useState(false)
 
+    const hamburgerClickHandler = async () => {
+      if(setCardDirection){
+        await setCardDirection('row')
+        setIsClicked1(!isClicked1)
+        setIsClicked2(false)
+      }
+    }
+    const gridClickHandler = async () => {
+      if(setCardDirection){
+        await setCardDirection('tile')
+        setIsClicked2(!isClicked2)
+        setIsClicked1(false)
+      }
+    }
+    const emotionalIconClickHandler = () => {
+      setIsClicked3(!isClicked3)
+    }
   return (
-    <SectionHeaderContainer>
+    <SectionHeaderContainer className='relative'>
       <div className='titleIcon'>{icon}</div>
       <div className='title'>{title}</div>
       <div>
         <ButtonContainer>
-          <GiHamburgerMenu className={'hover:cursor-pointer text-[24px]' + (isClicked1 ?' text-black':' text-gray-400' )} onClick={()=>{setIsClicked1(!isClicked1)}}/>
-          <BsGrid3X3GapFill className={'hover:cursor-pointer text-[24px]' + (isClicked2 ?` text-black`:' text-gray-400') } onClick={()=>{setIsClicked2(!isClicked2)}}/>
-          <EmotionalScore onClick={()=>{setIsClicked3(!isClicked3)
-          console.log(isClicked3)}}/>
+          {
+            type === 'hotNews' ? 
+            <div></div> :    
+            <>
+              <GiHamburgerMenu className={'hover:cursor-pointer text-[24px]' + (isClicked1 ?' text-black':' text-gray-400' )} onClick={hamburgerClickHandler}/>
+              <BsGrid3X3GapFill className={'hover:cursor-pointer text-[24px]' + (isClicked2 ?` text-black`:' text-gray-400') } onClick={gridClickHandler}/>
+              <EmotionalScoreIcon onClick={emotionalIconClickHandler}/>
+            </>
+          }
         </ButtonContainer>
       </div>
+      {isClicked3 && <EmotionalScoreModal className=''/>}
     </SectionHeaderContainer>
   )
 }
