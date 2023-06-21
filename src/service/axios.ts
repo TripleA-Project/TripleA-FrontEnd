a4c406fac634e6f39e218f3304752d6e9eced653
Ryan-TheLion <dae7249@nate.com>
2023-06-21 16:05:46 +0900

    feat: ci/cd

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -3,10 +3,10 @@ import { getURL } from '@/util/url';
 import { getCookie } from '@/util/cookies';
 
 function createAxiosInstance() {
-  const baseURL = process.env.NEXT_PUBLIC_SERVER ?? getURL();
-
+  // const baseURL = process.env.NEXT_PUBLIC_SERVER ?? getURL();
+  // 지금 환경변수를 바로 설정할 수 없을 것 같아 임시로 직접 작성함
   const axiosInstance = axios.create({
-    baseURL,
+    baseURL: 'http://54.180.102.131:8081',
     withCredentials: true,
   });
 
