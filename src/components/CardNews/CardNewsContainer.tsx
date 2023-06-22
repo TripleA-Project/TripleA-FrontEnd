e217939538894fd3f8eae326dd3dc94e0a47d3bd
youngeun <jessicak0828@gmail.com>
2023-06-22 11:37:23 +0900

    feat & design :  Card News 컴포넌트 카드 타입에 따라 style 다르게 적용

--- src/components/CardNews/CardNewsContainer.tsx
+++ src/components/CardNews/CardNewsContainer.tsx
@@ -5,11 +5,12 @@ import CardNews from './index'
 interface CardNewsContainerProps {
   newsArr: NewsData[],
   cardDirection: string,
+  type:string
 }
 
-export default function CardNewsContainer({newsArr, cardDirection}:CardNewsContainerProps) {
+export default function CardNewsContainer({newsArr, cardDirection, type}:CardNewsContainerProps) {
   return (
-    <div className='flex flex-wrap gap-[10px] m-auto'>
+    <div className={type==='hotNews'?'flex flex-wrap gap-[10px] m-auto justify-center':'flex flex-wrap gap-[10px] m-auto'}>
     {newsArr.map((news:NewsData)=>
       <CardNews cardDirection={cardDirection} key={news.newsId} newsId={news.newsId} symbol={news.symbol} source={news.source} title={news.title}  thumbnail={news.thumbnail} publishedDate={news.publishedDate} sentiment={news.sentiment} bookmark={news.bookmark}/>
     )}
