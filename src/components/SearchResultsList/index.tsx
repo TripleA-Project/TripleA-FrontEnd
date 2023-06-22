dccc3fbd207543cdf62f42fc76a6d0f60ecc455f
seon-mikim <tjsalsun@gmail.com>
2023-06-22 20:34:01 +0900

    feat: SearchResultsList 컴포넌트 생성

---
+++ src/components/SearchResultsList/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function SearchResultsList() {
+  return (
+    <div>
+      <div>
+        <span>최근 검색어</span>
+        <button>전체 삭제</button>
+      </div>
+      <div>
+        <div>최근 검색어가 없습니다.</div>
+      </div>
+    </div>
+  );
+}
+
+export default SearchResultsList;
