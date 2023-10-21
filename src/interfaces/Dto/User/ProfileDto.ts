8fb5e44bbe2b71036ad351165d5f850b6de8c189
Ryan-TheLion <dae7249@nate.com>
2023-10-21 17:59:46 +0900

    feat: nextPaymentDate 추가
    마이페이지 - 다음 결제 예정일 추가

--- src/interfaces/Dto/User/ProfileDto.ts
+++ src/interfaces/Dto/User/ProfileDto.ts
@@ -2,6 +2,8 @@ import { type APIResponse } from '../Core';
 import { type User } from '@/interfaces/User';
 
 // 유저 프로필
-export interface ProfilePayload extends Pick<User, 'email' | 'fullName' | 'membership'> {}
+export interface ProfilePayload extends Pick<User, 'email' | 'fullName' | 'membership'> {
+  nextPaymentDate?: string;
+}
 
 export interface ProfileResponse extends APIResponse<ProfilePayload> {}
