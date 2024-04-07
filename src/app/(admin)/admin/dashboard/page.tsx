2ae04e12e2de291458c742ab607091a5113e3223
Ryan-TheLion <dae7249@nate.com>
2024-04-07 21:23:59 +0900

    feat: 유저 통계
    - 기존 lightweight chart는 시간의 흐름에 따른
    주식 차트에만 적합한 것으로 생각되어
    re-chart-js 라이브러리를 이용하여
    유저 수 통계 차트를 구현함

--- src/app/(admin)/admin/dashboard/page.tsx
+++ src/app/(admin)/admin/dashboard/page.tsx
@@ -1,4 +1,9 @@
 import { Metadata } from 'next';
+import { Suspense } from 'react';
+import UserStatistics from './_components/UserStatistics';
+import MuiSpinner from '@/components/UI/Spinner/MuiSpinner';
+import { ErrorBoundary } from 'react-error-boundary';
+import DashBoardErrorBoundary from './_components/DashBoardErrorBoundary';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
@@ -9,7 +14,24 @@ export const metadata: Metadata = {
 };
 
 function AdminDashBoard() {
-  return <div>dashboard</div>;
+  return (
+    <div className="box-border p-4">
+      <div className="h-6" />
+      <ErrorBoundary FallbackComponent={DashBoardErrorBoundary}>
+        <Suspense
+          fallback={
+            <div className="box-border flex h-[calc(100vh-131px)] w-full items-center justify-center p-page px-4">
+              <div className="flex w-full items-center justify-center">
+                <MuiSpinner />
+              </div>
+            </div>
+          }
+        >
+          <UserStatistics />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  );
 }
 
 export default AdminDashBoard;
