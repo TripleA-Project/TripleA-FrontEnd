ad30b3e271a80bea02b251224917d553f5bfb91a
Ryan-TheLion <dae7249@nate.com>
2024-05-18 02:53:42 +0900

    feat: 무료체험 유저 프로필 페이지
    무료체험 유저의 프로필페이지인 경우
    무료체험 중 텍스트, 무료체험 기간으로
    멤버십 상태 컴포넌트 렌더링

--- src/components/Membership/MembershipInfo.tsx
+++ src/components/Membership/MembershipInfo.tsx
@@ -10,6 +10,7 @@ import { useState } from 'react';
 import UnsubscribeDialog from './Dialog/UnsubscribeDialog';
 import { toastNotify } from '@/util/toastNotify';
 import dayjs from 'dayjs';
+import { twMerge } from 'tailwind-merge';
 
 interface MembershipInfoProps {
   user?: ProfilePayload;
@@ -39,7 +40,12 @@ function MembershipInfo({ user }: MembershipInfoProps) {
     <div className="mb-8 mt-11 box-border flex items-center justify-between rounded-[10px] bg-[#F5F7F9] px-[18.68px] py-[21px]">
       <div className="pl-[20.82px]">
         <div className="mb-2.5 font-bold">멤버십 / 결제 정보</div>
-        <div className={`${user?.membership === 'BASIC' ? 'mb-1.5' : ''}`}>
+        <MembershipStatus
+          onUnSubscribe={() => setOpenUnsubscribeDialog(true)}
+          unSubscribeStatus={unSubscribeStatus}
+          {...user}
+        />
+        {/* <div className={`${user?.membership === 'BASIC' ? 'mb-1.5' : ''}`}>
           <span className="font-semibold text-[#5B6267]">현재 구독 상태 : </span>
           <span className="font-semibold text-[#FD954A]">
             {!user?.membership ? '' : user?.membership === 'BASIC' ? '일반회원' : '유료회원'}
@@ -63,7 +69,7 @@ function MembershipInfo({ user }: MembershipInfoProps) {
               해지하기
             </button>
           </>
-        ) : null}
+        ) : null} */}
       </div>
       <Link href="/mypage/membership">
         <HiOutlineChevronRight className="shrink-0 text-2xl text-[#FD954A]" />
@@ -79,3 +85,100 @@ function MembershipInfo({ user }: MembershipInfoProps) {
 }
 
 export default MembershipInfo;
+
+const MembershipStatus = ({
+  freeTrial,
+  freeTierStartDate,
+  freeTierEndDate,
+  membership,
+  nextPaymentDate,
+  onUnSubscribe,
+  unSubscribeStatus,
+}: Partial<ProfilePayload> & { onUnSubscribe: () => void; unSubscribeStatus: any }) => {
+  return (
+    <>
+      <SubscribeStatus freeTrial={freeTrial} membership={membership} />
+      <SubscribeDateStatus
+        freeTrial={freeTrial}
+        freeTierStartDate={freeTierStartDate}
+        freeTierEndDate={freeTierEndDate}
+        membership={membership}
+        nextPaymentDate={nextPaymentDate}
+        onUnSubscribe={onUnSubscribe}
+        unSubscribeStatus={unSubscribeStatus}
+      />
+    </>
+  );
+};
+
+const SubscribeStatus = ({ freeTrial, membership }: Partial<Pick<ProfilePayload, 'freeTrial' | 'membership'>>) => {
+  const subscribeWrapperClassNames = twMerge([!freeTrial && membership === 'BASIC' ? 'mb-1.5' : '']);
+
+  if (freeTrial) {
+    return (
+      <div className={subscribeWrapperClassNames}>
+        <span className="font-semibold text-[#5B6267]">현재 구독 상태 : </span>
+        <span className="font-semibold text-[#FD954A]">무료체험 이용 중</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className={subscribeWrapperClassNames}>
+      <span className="font-semibold text-[#5B6267]">현재 구독 상태 : </span>
+      <span className="font-semibold text-[#FD954A]">
+        {!membership ? '' : membership === 'BASIC' ? '일반회원' : '유료회원'}
+      </span>
+    </div>
+  );
+};
+
+const SubscribeDateStatus = ({
+  freeTrial,
+  freeTierStartDate,
+  freeTierEndDate,
+  membership,
+  nextPaymentDate,
+  onUnSubscribe,
+  unSubscribeStatus,
+}: Partial<
+  Pick<ProfilePayload, 'freeTrial' | 'membership' | 'nextPaymentDate' | 'freeTierStartDate' | 'freeTierEndDate'>
+> & {
+  onUnSubscribe: () => void;
+  unSubscribeStatus: any;
+}) => {
+  if (freeTrial) {
+    return (
+      <div className="mb-1.5 mt-1.5">
+        <span className="font-semibold text-[#5B6267]">무료체험 기간 : </span>
+        <span className="font-semibold text-[#FD954A]">
+          {freeTierStartDate && freeTierEndDate
+            ? `${dayjs(freeTierStartDate).format('YYYY/MM/DD')} ~ ${dayjs(freeTierEndDate).format('YYYY/MM/DD')}`
+            : ''}
+        </span>
+      </div>
+    );
+  }
+
+  if (membership === 'PREMIUM') {
+    return (
+      <>
+        <div className="mt-1.5">
+          <span className="font-semibold text-[#5B6267]">다음 결제일 : </span>
+          <span className="font-semibold text-[#FD954A]">
+            {nextPaymentDate ? dayjs(nextPaymentDate).format('YYYY/MM/DD') : ''}
+          </span>
+        </div>
+        <button
+          className="mb-1.5 mt-[15px] border-b-2 border-b-[#777777] text-sm font-semibold text-[#777777] disabled:cursor-not-allowed disabled:text-violet-400/60"
+          disabled={unSubscribeStatus === 'loading'}
+          onClick={onUnSubscribe}
+        >
+          해지하기
+        </button>
+      </>
+    );
+  }
+
+  return null;
+};
